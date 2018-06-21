70a66f329ef4ac2b5be02f21327587b93888e3b0
LAPPY\keith <wongdaokang@gmail.com>
2018-06-21 20:26:10 +0800

    - Updated scenes to work with 16:9 - Added highscore and shop scenes

--- Assets/Scripts/Menu Scripts/MainMenu.cs
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     //When start button is clicked
-    private void PlayGame()
+    public void PlayGame()
     {
+        SceneManager.LoadScene("Game");
     }
 
     //When highscore button is clicked
     public void Highscore()
     {
+        SceneManager.LoadScene("Highscore");
+    }
+
+    //When shop button is clicked
+    public void Shop()
+    {
+        SceneManager.LoadScene("Shop");
     }
 
     //When exit button is clicked
     public void Quit()
     {
-        Application.Quit();
-        Debug.Log("Clicked");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
     }
 }
