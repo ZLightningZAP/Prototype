16fc479e496a8bed79159b78a46d116cf2ad842b
LAPPY\keith <wongdaokang@gmail.com>
2018-06-21 19:38:25 +0800

    - Splash screen added - Fade from splash to main menu added

---
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    //When start button is clicked
+    private void PlayGame()
+    {
+    }
+
+    //When highscore button is clicked
+    public void Highscore()
+    {
+    }
+
+    //When exit button is clicked
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Clicked");
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/Splash.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    private Image Splashscreen;
+
+    private bool FadedIn = false;
+    private bool FadedOut = false;
+
+    public int FadeInSeconds;
+    public int FadeOutSeconds;
+
+    // Use this for initialization
+    void Start()
+    {
+        Splashscreen = GetComponent<Image>();
+        //Set Alpha to zero for fade in
+        Splashscreen.canvasRenderer.SetAlpha(0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Fade In
+        if (Splashscreen.canvasRenderer.GetAlpha() < 0.95f && FadedIn == false)
+        {
+            FadeIn();
+            if (Splashscreen.canvasRenderer.GetAlpha() >= 0.95f)
+            {
+                FadedIn = true;
+            }
+        }
+
+        //Fade Out
+        if (FadedIn == true && FadedOut == false)
+        {
+            FadeOut();
+            if (Splashscreen.canvasRenderer.GetAlpha() <= 0.10f)
+            {
+                FadedOut = true;
+            }
+        }
+
+        //Load Next Scene when done
+        if (FadedIn == true && FadedOut == true)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+    //Fade In
+    private void FadeIn()
+    {
+        Splashscreen.CrossFadeAlpha(1f, FadeInSeconds, false);
+    }
+
+    //Fade Out
+    private void FadeOut()
+    {
+        Splashscreen.CrossFadeAlpha(0f, FadeOutSeconds, false);
+    }
+}
